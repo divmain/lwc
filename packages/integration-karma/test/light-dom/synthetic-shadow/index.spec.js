ffb4e22e5fba57b0f52406c9bc6ec5a33ff6aee8
Rui Qiu <rui.qiu@salesforce.com>
2021-10-22 15:57:25 -0700

    fix(synthetic-shadow): synthetic shadow doesn't preserve assignedSlot behavio
    Fixes #2377
    
    W-9492998

--- packages/integration-karma/test/light-dom/synthetic-shadow/index.spec.js
+++ packages/integration-karma/test/light-dom/synthetic-shadow/index.spec.js
@@ -24,11 +24,9 @@ describe('Light DOM + Synthetic Shadow DOM', () => {
             expect(nodes.slot.assignedElements()).toEqual([nodes.p]);
         });
 
-        if (!process.env.MIXED_SHADOW) {
-            it('assignedSlot', () => {
-                expect(nodes.p.assignedSlot).toEqual(nodes.slot);
-            });
-        }
+        it('assignedSlot', () => {
+            expect(nodes.p.assignedSlot).toEqual(nodes.slot);
+        });
 
         it('childNodes', () => {
             expect(Array.from(nodes.slot.childNodes)).toEqual([]);
@@ -195,13 +193,9 @@ describe('Light DOM + Synthetic Shadow DOM', () => {
         it('assignedElements', () => {
             expect(nodes.slot.assignedElements()).toEqual([nodes.p]);
         });
-
-        if (!process.env.MIXED_SHADOW) {
-            it('assignedSlot', () => {
-                expect(nodes.p.assignedSlot).toEqual(nodes.slot);
-            });
-        }
-
+        it('assignedSlot', () => {
+            expect(nodes.p.assignedSlot).toEqual(nodes.slot);
+        });
         it('childNodes', () => {
             expect(Array.from(nodes.slot.childNodes)).toEqual([]);
         });
